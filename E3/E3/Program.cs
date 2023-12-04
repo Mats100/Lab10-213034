2d6325217adb49ab39ff0e0acc0180362fca635d
Mats100 <Muneeb75@icloud.com>
2023-12-04 12:07:17 +0500

    Committed

---
+++ E3/E3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml.Linq;
+
+namespace Operators
+{
+    class Pet
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class Concat
+    {
+        static Pet[] GetCats()
+        {
+            Pet[] cats = { new Pet { Name = "Barley", Age = 8 },
+                           new Pet { Name = "Boots", Age = 4 },
+                           new Pet { Name = "Whiskers", Age = 1 } };
+            return cats;
+        }
+        static Pet[] GetDogs()
+        {
+            Pet[] dogs = { new Pet { Name = "Bounder", Age = 3 },
+                           new Pet { Name = "Snoopy", Age = 14 },
+                           new Pet { Name = "Fido", Age = 9 } };
+            return dogs;
+        }
+        static void Main(string[] args)
+        {
+            Pet[] cats = GetCats();
+            Pet[] dogs = GetDogs();
+            IEnumerable<string> query = cats.Select(cat => cat.Name).Concat(dogs.Select(dog => dog.Name));
+            foreach (var e in query)
+            {
+                Console.WriteLine("Name = {0}", e);
+            }
+            Console.WriteLine("\n Press any key to continue."); Console.ReadKey();
+        }
+    }
+}
